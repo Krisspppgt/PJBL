ef16ace04b920307fb32f069937440cb355bfbf6
Ramabotak <muhammadrizqyramdhan2@gmail.com>
2025-11-12 10:51:19 +0700

    new database seeder

--- database/migrations/2025_11_12_031726_create_spot_table.php
+++ database/migrations/2025_11_12_031726_create_spot_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('spot', function (Blueprint $table) {
             $table->id();
+            $table->string('nama')->unique();
             $table->timestamps();
         });
     }
