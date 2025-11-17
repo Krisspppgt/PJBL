e95d79d43a5fc88da53dba22f5795ffea362feae
Ramabotak <muhammadrizqyramdhan2@gmail.com>
2025-11-17 09:44:40 +0700

    welcome blde

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'ms-3 mb-3 rounded w-full text-center text-white bg-gradient-to-r from-blue-600 via-red-500 to-blue-900 hover:scale-95 transition duration-500 justify-center']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'ms-3 mb-3 rounded w-full text-center text-white bg-gradient-to-r from-blue-300 to-blue-900 hover:scale-95 transition duration-500 justify-center']) }}>
     {{ $slot }}
 </button>
